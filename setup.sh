8e2a7bced0a6877ff345b52525c2253ded6f83c6
gloffler <g.loffler@icloud.com>
2024-06-27 12:29:21 +0200

    cleaning up

--- setup.sh
+++ setup.sh
@@ -3,27 +3,29 @@
 # Variables
 DNF_CMD=$(type dnf 2> /dev/null)
 APT_CMD=$(type apt-get 2> /dev/null)
-
 TOOLS="vim nano git fish"
 VIM_CONFIG="set ts=2 ai expandtab"
 VIM_RC=$HOME/.vimrc
 
-# Install Tools
-echo "Updating system..."
-
-if [[ ! -z $DNF_CMD ]]; then    
+if [[ ! -z $DNF_CMD ]]; then
+  echo "Updating system..."
   sudo dnf -y update
-  INSTALL_COMPLETE=$(sudo dnf install $TOOLS)
+  echo "Installing tools..."
+  sudo dnf install $TOOLS
+  INSTALL_COMPLETE=$?
 elif [[ ! -z $APT_CMD ]]; then
+  echo "Updating system..."
   sudo apt-get update
-  INSTALL_COMPLETE=$(sudo apt-get install $TOOLS)
+  echo "Installing tools..."
+  sudo apt-get install $TOOLS
+  INSTALL_COMPLETE=$?
 else
   echo "Error: No package manager found!"
   exit 1;
 fi
 
-if [[ ! -z $INSTALL_COMPLETE ]]; then
-  if grep -wq "$VIM_CONFIG" $VIM_RC; then
+if [[ $INSTALL_COMPLETE -eq 0 ]]; then
+  if grep -wq "$VIM_CONFIG" $VIM_RC 2> /dev/null; then
     echo "VIM already configured!"
   else
     echo "Configuring VIM..."
